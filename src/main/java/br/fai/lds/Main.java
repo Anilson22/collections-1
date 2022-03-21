9dbca0189cb3cc3928672d27414341bbd153b984
Anilson22 <pcassioferreira@gmail.com>
2022-03-21 01:10:36 -0300

    feat: Finalizado Projeto foi add 2 itens na lista e foi removido 2

--- src/main/java/br/fai/lds/Main.java
+++ src/main/java/br/fai/lds/Main.java
@@ -1,5 +1,9 @@
 package br.fai.lds;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -7,8 +11,49 @@ public class Main {
 		app.start();
 
 	}
-	private static void start() {
+	private void start() {
+		int i = 0;
+		List<String> valoresDigitados = new ArrayList<String>();
 		
+		while (i < 5) {
+			String valorDigitado = "";
+
+			System.out.print("Digite algum valor: ");
+			valorDigitado = obterDados();
+			
+			if (!valorDigitado.trim().isEmpty() && !valorDigitado.equals("INVALIDO")) {
+				valoresDigitados.add(valorDigitado);
+				i++;
+			}
+		}
+		
+		valoresDigitados.add("Item adicionado automaticamente 01");
+		valoresDigitados.add("Item adicionado automaticamente 02");
+		
+		exibeValores(valoresDigitados);
+		
+		valoresDigitados.remove(1);
+		valoresDigitados.remove(1);
+		
+		exibeValores(valoresDigitados);
+	}
+	
+	private String obterDados() {
+		Scanner scanner = new Scanner(System.in);
+		String valor;
+		try {
+			valor = scanner.nextLine();
+		} catch (Exception e) {
+			valor = "INVALIDO";
+		}
+		
+		return valor;
+	}
+	private void exibeValores(List<String> lista) {
+		System.out.println("\n\nExibindo valores da lista.");
+		for (String valor: lista) {
+			System.out.println(valor);
+		}
 	}
 
 }
